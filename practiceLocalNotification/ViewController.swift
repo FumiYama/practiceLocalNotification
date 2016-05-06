a8b91385b4c92476a5955c45c13c79b4ecd138e8
FumiYama <f.yamanaka1127@gmail.com>
2016-05-06 18:36:59 +0900

    Initial Commit

---
+++ practiceLocalNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practiceLocalNotification
+//
+//  Created by Fumiya Yamanaka on 2016/05/06.
+//  Copyright © 2016年 fumiya yamanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
